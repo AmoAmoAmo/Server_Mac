7c3ba60f15b9e0b2dde583dbdca728e417f7dd6b
AmoAmoAmo <ohmisszhang@163.com>
2017-07-14 20:24:32 +0800

    Initial Commit

---
+++ Mac_GUI_Test/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Mac_GUI_Test
+//
+//  Created by Josie on 2017/7/14.
+//  Copyright © 2017年 Josie. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
